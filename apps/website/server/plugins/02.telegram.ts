030f751ccd4d57f21088445b4c5fdddefd25c611
Nick Kosarev <hmbanan666@hotmail.com>
2025-04-11 13:13:50 +0200

    feat: new bot for orders

--- apps/website/server/plugins/02.telegram.ts
+++ apps/website/server/plugins/02.telegram.ts
@@ -1,4 +1,5 @@
 import { useCreateBot } from '../services/telegram/bot'
+import { useCreateOrderBot } from '../services/telegram/orderBot'
 
 /**
  * Start Telegram bots (using long polling)
@@ -12,12 +13,13 @@ export default defineNitroPlugin(() => {
   const logger = useLogger('plugin-start-telegram')
   const { telegram } = useRuntimeConfig()
 
-  if (!telegram.botToken) {
+  if (!telegram.botToken || !telegram.orderBotToken) {
     // No config provided
     return
   }
 
   useCreateBot()
+  useCreateOrderBot()
 
   logger.success('Telegram bots started')
 })
